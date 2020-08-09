d25432e5b0a693cc9e5611d9705266c685a080cf
Eric O'Neill <oneill.eric23@gmail.com>
2020-08-08 19:34:05 -0600

    Update/fix font issue - change to League Spartan

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.scss';
+
+function NavBar() {
+  return (
+    <nav className="nav">
+      <p>NAV BAR</p>
+    </nav>
+  )
+}
+
+export default NavBar;
