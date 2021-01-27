8a25af9df138ec8e24109e33c1cdafd2f1ffcf51
Eric O'Neill <oneill.eric23@gmail.com>
2021-01-26 21:01:09 -0700

    Get menu to slide in FINALLY

--- src/Components/Hamburger/Hamburger.js
+++ src/Components/Hamburger/Hamburger.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Hamburger.scss';
 
 const HamburgerMenu = ({toggleMenu, isMenuDisplayed}) => {
-  const visibility = isMenuDisplayed ? 'burger-menu-hide' : 'burger-menu-show';
+  // const visibility = isMenuDisplayed ? 'burger-menu-hide' : 'burger-menu-show';
+  const visibility = isMenuDisplayed ? 'burger-menu-show' : 'burger-menu-hide'
 
   return (
-    <nav 
+    <nav
       className={visibility} 
       onClick={() => {toggleMenu()}}
       >
