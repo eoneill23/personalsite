c256b0f45c41b11a8f8b654812af3778185626ca
Eric O'Neill <oneill.eric23@gmail.com>
2021-01-28 20:11:21 -0700

    Begin work on animation for hamburger menu

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,13 +3,19 @@ import './NavBar.scss';
 import hamburger from '../../assets/hamburger.svg';
 
 const NavBar = ({toggleMenu, isMenuDisplayed}) => {
-  const visibility = isMenuDisplayed ? 'nav-opaque' : 'nav'
+  const visibility = isMenuDisplayed ? 'nav-opaque' : 'nav';
+  const spanActive = isMenuDisplayed ? 'span-active' : 'span-inactive';
 
   return (
     <nav className={visibility}>
       <h1 className="main-title">ERIC O'NEILL</h1>
-      <div className="link-container">
-        <img src={hamburger} onClick={() => { toggleMenu() }} className="burger"/>
+      <div className="link-container" onClick={() => { toggleMenu() }}>
+        
+          <span className={spanActive} id="nav-icon1"></span>
+          <span className={spanActive} id="nav-icon2"></span>
+          <span className={spanActive} id="nav-icon3"></span>
+        
+        {/* <img src={hamburger} onClick={() => { toggleMenu() }} className="burger"/> */}
         <a href="#home" className="nav-link">Home</a>
         <a href="#skills" className="nav-link">Skills/GitHub</a>
         <a href="#contact" className="nav-link">Contact</a>
